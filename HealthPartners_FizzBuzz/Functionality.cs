ffe03a21ecf8add4616761e06786ba3e79a51082
Nancy van Blerk <nonnavBlerk@gmail.com>
2024-05-10 12:36:26 +0100

    Initial structure design

---
+++ HealthPartners_FizzBuzz/Functionality.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthPartners_FizzBuzz
+{
+    internal class Functionality
+    {
+        public StringBuilder MyOwn() 
+        {
+            StringBuilder sb = new(); 
+
+
+
+            return sb;
+        }
+
+        public StringBuilder StackOverflow() 
+        {
+            StringBuilder sb = new();
+
+            return sb;
+        }
+
+        public StringBuilder DocExample() 
+        {
+            StringBuilder sb = new();
+
+            return sb;
+        }
+    }
+
+}
--- HealthPartners_FizzBuzz/Program.cs
+++ HealthPartners_FizzBuzz/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HealthPartners_FizzBuzz;
+using System.Text;
+using static HealthPartners_FizzBuzz.Functionality;
+
+//I haven't done this in a few years so I've done some research and found a lot of cool solutions
+
+
+//I'll still be doing my own attmept but like to experiment so I've included some of those
+//solutions to see how they work. Feel free to use their options if you're curious
+Functionality func = new();
+StringBuilder funcBob = new StringBuilder();
+StringBuilder sb = new();
+sb.AppendLine("Please select:");
+sb.AppendLine("1. My own attempt");
+sb.AppendLine("2. StackOverflow example");
+sb.AppendLine("3. Example in document");
+
+Console.WriteLine(sb);
+string read = Console.ReadLine() ?? "noInputError";
+
+try
+{
+    if (read != null && string.IsNullOrEmpty(read))
+    {
+        switch (read)
+        {
+            case "1":
+                funcBob = func.MyOwn();
+                break;
+            case "2":
+                funcBob = func.StackOverflow();
+                break;
+            case "3":
+                funcBob = func.DocExample();
+                break;
+            default:
+                funcBob.AppendLine("Please select between 1 - 3");
+                break;
+        }
+
+        Console.WriteLine(funcBob);
+    }
+    else
+    {
+        Console.WriteLine("ReadLine was not properly returned to the code");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine(ex.InnerException);
+}
